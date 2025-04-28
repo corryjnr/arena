2d1a710559dfb145ad44d1d2bffac2396f32854a
NELSON OMOI <nelsonomoi@NELSONs-Laptop.local>
2025-04-28 06:54:14 +0300

    fix- issue-2 bootstrap spring boot app

---
+++ src/test/java/com/kenya/jug/arena/ArenaApplicationTests.java
@@ -0,0 +1,15 @@
+package com.kenya.jug.arena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class ArenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
